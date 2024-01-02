7453aa90c89dcd6e286065ee0072d44572ddd045
jhofrandev <jhofran.dev@outlook.com>
2024-01-02 09:05:35 -0500

    add changes css

--- js/mokepon.js
+++ js/mokepon.js
@@ -25,7 +25,7 @@
     sectionSeleccionarMascota.style.display = 'none'
 
     let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
-    sectionSeleccionarAtaque.style.display = 'block'
+    sectionSeleccionarAtaque.style.display = 'flex'
 
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
